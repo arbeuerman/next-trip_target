843f31bd3cd1a35147c9f4ea133a865d2ac3991f
Alexandra Rodriguez-Beuerman <alexandra@classkick.com>
2022-08-26 07:15:16 -0500

    added api calls react router and new css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import './App.css';
+import RouteSelector from './components/RouteSelector';
+import Home from './components/Home';
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn to spell
-        </a>
+        Next Trip
+        <NavBar/>
       </header>
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/route-selector' element={<RouteSelector/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
